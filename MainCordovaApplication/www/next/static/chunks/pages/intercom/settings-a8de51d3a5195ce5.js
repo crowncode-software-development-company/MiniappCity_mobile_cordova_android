9de057bb847036cb60da2fafb16eeb7da0ac3acd
A14X4Y <leshabelousov2001@gmail.com>
2024-02-20 14:45:17 +0500

    feat(cordova): add plugins device

---
+++ MainCordovaApplication/www/next/static/chunks/pages/intercom/settings-a8de51d3a5195ce5.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[749],{99453:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/intercom/settings",function(){return u(64873)}])}},function(n){n.O(0,[774,888,179],function(){return n(n.s=99453)}),_N_E=n.O()}]);
